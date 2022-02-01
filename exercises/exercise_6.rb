a2156ddc6ba69ffa6a41bf4ec8663ccfb3942059
Mohamed Mohamed <justmoe23@gmail.com>
2022-02-01 02:44:11 +0000

    complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create([
+  { first_name: "Khurram", last_name: "Virani", hourly_rate: 60 },
+  { first_name: "Addy", last_name: "Soppit", hourly_rate: 55 },
+  { first_name: "Levi", last_name: "Beton", hourly_rate: 50 },
+  { first_name: "Loria", last_name: "Avery", hourly_rate: 45 },
+])
+
+@store2.employees.create([
+  { first_name: "Kristal", last_name: "Sturm", hourly_rate: 75 },
+  { first_name: "Nikolia", last_name: "Kitchinghan", hourly_rate: 70 },
+  { first_name: "Paolo", last_name: "Sedcole", hourly_rate: 65 },
+  { first_name: "Karrie", last_name: "Pratten", hourly_rate: 60 },
+])
